e81ebea78328a88baf8d698ef65fb80f86abc79f
FlameK <47636477+FlameK@users.noreply.github.com>
2020-01-13 22:56:47 +0100

    Create HandleLooting.java

---
+++ Scripts/MythsGuildGDK/Tasks/HandleLooting.java
@@ -0,0 +1,113 @@
+package scripts.MythsGuildGDK.Tasks;
+
+import org.tribot.api.General;
+import org.tribot.api2007.*;
+import org.tribot.api2007.types.RSGroundItem;
+import org.tribot.api2007.types.RSItem;
+import scripts.entityselector.*;
+import scripts.entityselector.finders.prefabs.GroundItemEntity;
+import scripts.MythsGuildGDK.framework.Priority;
+import scripts.MythsGuildGDK.framework.Task;
+import scripts.entityselector.finders.prefabs.ItemEntity;
+
+public class HandleLooting implements Task {
+
+    // if anything fucks up it's because I changed these to private
+
+    private RSGroundItem[] loot = Entities.find(GroundItemEntity::new).nameEquals("Green dragonhide", "Dragon bones", "Nature rune",
+            "Grimy ranarr weed", "Shield left half", "Dragon spear", "Ensouled dragon head")
+            .sortByDistance()
+            .actionsEquals("Take")
+            .getResults();
+
+    private String[] dragonLoot = {"Green dragonhide", "Dragon bones", "Nature rune",
+            "Grimy ranarr weed", "Shield left half", "Dragon spear", "Ensouled dragon head"};
+    private static String[] food = {"Lobster"};
+    private String[] dragon = {"Green dragon"};
+
+
+    @Override
+    public Priority priority() {
+        return Priority.MEDIUM;
+    }
+
+    @Override
+    public String toString() {
+        return "Looting " + "Status: " + getTaskState().name();
+    }
+
+    @Override
+    public boolean validate() {
+        return GroundItems.find(dragonLoot).length > 0 && (!Inventory.isFull() || (Inventory.isFull() && Inventory.find(food).length > 0));
+
+    }
+
+    @Override
+    public void execute() {
+
+        switch (getTaskState()) {
+
+
+            case MAKING_SPACE_FOR_LOOT: //improve this by making it eat the required amount to make space for food before looting
+
+                ItemEntity isEdible = Entities.find(ItemEntity::new)
+                        .actionsEquals("Eat");
+
+
+
+                if (isEdible != null) {
+                    RSItem[] edible = Inventory.find(food);
+                    if (edible != null && edible.length > 0) {
+                        if (edible[0].click("Eat")) {
+                            General.sleep(General.randomSD(750, 45));
+                        }
+                        System.out.println("Eating food to create inventory space, new health is now:" + Combat.getHP());
+                    }
+                }
+                    break;
+
+            case LOOTING:
+                if (!Inventory.isFull() && Inventory.find(food).length >= 0) {
+                    if (loot != null) {
+                        RSGroundItem[] groundLoot = GroundItems.find(dragonLoot);
+                        if (groundLoot != null & groundLoot.length > 0) {
+                            if (groundLoot[0].click("Take " + groundLoot[0].getDefinition().getName())) {
+                                General.sleep(General.randomSD(430, 80));
+                            }
+                            System.out.println("Looting items...");
+                        }
+                    }
+
+                    break;
+                }
+
+
+            case IDLING: //need to change to something less pointless
+
+                if (dragonLoot.length == 0 && NPCs.find(dragon) == null) {
+                    General.sleep(1200, 2000);
+                }
+                break;
+        }
+    }
+
+    private enum TaskState {
+
+        MAKING_SPACE_FOR_LOOT,
+        LOOTING,
+        IDLING
+
+    }
+
+    private TaskState getTaskState() {
+
+        if (GroundItems.find(dragonLoot).length > 0 && Inventory.isFull() && Inventory.find(food).length > 0){
+            return TaskState.MAKING_SPACE_FOR_LOOT;
+        }
+        if (GroundItems.find(dragonLoot).length > 0 && !Inventory.isFull()) {
+            return TaskState.LOOTING;
+        }
+        return TaskState.IDLING;
+
+    }
+}
