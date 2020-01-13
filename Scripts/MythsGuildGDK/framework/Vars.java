eeaabdfa16763375e98b86ba609e5f822267dc0a
FlameK <47636477+FlameK@users.noreply.github.com>
2020-01-13 22:59:14 +0100

    Create Vars.java

---
+++ Scripts/MythsGuildGDK/framework/Vars.java
@@ -0,0 +1,19 @@
+package scripts.MythsGuildGDK.framework;
+
+public class Vars {
+	
+	
+		private static Vars vars;
+		
+		public static Vars get() {
+			return vars == null? vars = new Vars() : vars;
+		}
+		
+		public static void reset() {
+			vars = new Vars();
+		}
+		
+		// Script information
+		public String status = "Initializing";
+		public long runTime;
+}
