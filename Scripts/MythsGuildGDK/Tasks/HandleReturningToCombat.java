f48074ffe2536d8ed2a33f4c330f06a0fb9b3bb5
FlameK <47636477+FlameK@users.noreply.github.com>
2020-01-22 17:28:43 +0100

    Update HandleReturningToCombat.java

--- Scripts/MythsGuildGDK/Tasks/HandleReturningToCombat.java
+++ Scripts/MythsGuildGDK/Tasks/HandleReturningToCombat.java
@@ -20,7 +20,7 @@ public class HandleReturningToCombat implements Task {
     private static RSTile bottomOfLadder = new RSTile(1936, 9009, 1);
 
     private static final RSArea dungeon = new RSArea(new RSTile [] { new RSTile(1937, 9014, 1),new RSTile(1921, 9014, 1),new RSTile(1921, 8995, 1),new RSTile(1931, 8987, 1),new RSTile(1937, 8982, 1),new RSTile(1941, 8986, 1),new RSTile(1943, 8987, 1),new RSTile(1946, 8989, 1),new RSTile(1951, 8996, 1),new RSTile(1950, 9003, 1) });
-    private static final RSArea dragonArea = new RSArea(new RSTile[]{new RSTile(1947, 8987, 1), new RSTile(1936, 8987, 1), new RSTile(1936, 8994, 1), new RSTile(1939, 8994, 1), new RSTile(1941, 8995, 1), new RSTile(1943, 8997, 1), new RSTile(1944, 8999, 1), new RSTile(1945, 9000, 1), new RSTile(1946, 9000, 1), new RSTile(1947, 9000, 1), new RSTile(1948, 9000, 1), new RSTile(1949, 8999, 1), new RSTile(1950, 8998, 1), new RSTile(1950, 8997, 1), new RSTile(1949, 8995, 1), new RSTile(1948, 8994, 1), new RSTile(1947, 8994, 1), new RSTile(1946, 8993, 1)});
+    public static final RSArea dragonArea = new RSArea(new RSTile [] { new RSTile(1937, 9000, 1),new RSTile(1944, 9006, 1),new RSTile(1954, 8999, 1),new RSTile(1951, 8992, 1),new RSTile(1947, 8986, 1),new RSTile(1940, 8986, 1),new RSTile(1937, 8982, 1),new RSTile(1935, 8979, 1),new RSTile(1927, 8978, 1),new RSTile(1924, 8999, 1) });
     private static final RSArea greenDragonArea = new RSArea(new RSTile [] { new RSTile(1946, 8989, 1),new RSTile(1941, 8989, 1),new RSTile(1941, 8996, 1),new RSTile(1943, 8998, 1),new RSTile(1944, 8998, 1),new RSTile(1944, 8999, 1),new RSTile(1944, 9000, 1),new RSTile(1945, 9001, 1),new RSTile(1946, 9001, 1),new RSTile(1947, 9000, 1),new RSTile(1948, 8999, 1),new RSTile(1949, 8999, 1),new RSTile(1950, 8998, 1),new RSTile(1950, 8997, 1),new RSTile(1949, 8996, 1),new RSTile(1947, 8996, 1),new RSTile(1946, 8995, 1),new RSTile(1946, 8994, 1),new RSTile(1945, 8993, 1),new RSTile(1945, 8991, 1),new RSTile(1946, 8991, 1) });
     private static final RSArea mythsTeleportArea = new RSArea(new RSTile [] { new RSTile(2456, 2852, 0),new RSTile(2458, 2852, 0),new RSTile(2459, 2851, 0),new RSTile(2460, 2851, 0),new RSTile(2461, 2850, 0),new RSTile(2461, 2849, 0),new RSTile(2461, 2848, 0),new RSTile(2461, 2847, 0),new RSTile(2461, 2846, 0),new RSTile(2461, 2845, 0),new RSTile(2461, 2844, 0),new RSTile(2459, 2843, 0),new RSTile(2458, 2842, 0),new RSTile(2456, 2842, 0),new RSTile(2455, 2843, 0),new RSTile(2454, 2843, 0),new RSTile(2453, 2844, 0),new RSTile(2453, 2845, 0),new RSTile(2453, 2846, 0),new RSTile(2453, 2847, 0),new RSTile(2453, 2848, 0),new RSTile(2453, 2849, 0),new RSTile(2453, 2850, 0),new RSTile(2454, 2851, 0) });
     private static final RSArea bottomOfLadderArea = new RSArea(new RSTile [] { new RSTile(1935, 9007, 1),new RSTile(1935, 9012, 1),new RSTile(1940, 9012, 1),new RSTile(1939, 9007, 1) });
@@ -37,7 +37,7 @@ public class HandleReturningToCombat implements Task {
 
     @Override
     public boolean validate() {
-        return (!Banking.isBankScreenOpen() && !dragonArea.contains(Player.getPosition()) && Inventory.getCount(food) == 15 && Inventory.find(dragonLoot).length == 0) || (bottomOfLadder.isOnScreen() && !dragonArea.contains(Player.getPosition()));
+        return (!Banking.isBankScreenOpen() && !dragonArea.contains(Player.getPosition()) && Inventory.getCount(food) == 5 && Inventory.find(dragonLoot).length == 0) || (bottomOfLadder.isOnScreen() && !dragonArea.contains(Player.getPosition()));
     }
 
     @Override
@@ -56,7 +56,7 @@ public class HandleReturningToCombat implements Task {
                     if (capeReturn != null && capeReturn.length > 0) {
                         if (capeReturn[0].click("Teleport")) {
                             Timing.waitCondition(() -> {
-                                General.sleep(General.randomSD(2000, 30));
+                                    General.sleep(General.randomSD(3000, 30));
                                 return mythsTeleportArea.contains(Player.getPosition());
                             }, General.random(300, 400));
                         }
@@ -116,10 +116,10 @@ private enum TaskState {
 
     private TaskState getTaskState () {
 
-        if (!dragonArea.contains(Player.getPosition()) && !mythsTeleportArea.contains(Player.getPosition()) && !bottomOfLadderArea.contains(Player.getPosition()) && Inventory.getCount(food) == 15 && Inventory.find(dragonLoot).length == 0 && !Banking.isBankScreenOpen()) {
+        if (!dragonArea.contains(Player.getPosition()) && !mythsTeleportArea.contains(Player.getPosition()) && !bottomOfLadderArea.contains(Player.getPosition()) && Inventory.getCount(food) == 5 && Inventory.find(dragonLoot).length == 0 && !Banking.isBankScreenOpen()) {
             return TaskState.TELEPORTING_TO_MYTHS_GUILD;
         }
-        if (mythsTeleportArea.contains(Player.getPosition()) && !bottomOfLadderArea.contains(Player.getPosition()) && Inventory.getCount(food) == 15 && Inventory.find(dragonLoot).length == 0) {
+        if (mythsTeleportArea.contains(Player.getPosition()) && !bottomOfLadderArea.contains(Player.getPosition()) && Inventory.getCount(food) == 5 && Inventory.find(dragonLoot).length == 0) {
             return TaskState.ENTERING_MYTHS_DUNGEON;
         }
         if (bottomOfLadderArea.contains(Player.getPosition()) && Inventory.find(food).length > 0) { // if the current one is buggy, this was the original option... Inventory.find(food).length == 15 && Inventory.find(dragonLoot).length == 0) {
