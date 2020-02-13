b3a88eff696d312d226d8df1e9b83b37f7334339
FlameK <47636477+FlameK@users.noreply.github.com>
2020-02-13 08:57:47 +0100

    Update fairyRingHandler.java

--- api/fairyRingHandler/fairyRingHandler.java
+++ api/fairyRingHandler/fairyRingHandler.java
@@ -4,6 +4,7 @@ import org.tribot.api.General;
 import org.tribot.api.Timing;
 import org.tribot.api2007.Interfaces;
 import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSInterface;
 import org.tribot.api2007.types.RSInterfaceChild;
 import org.tribot.api2007.types.RSVarBit;
 
@@ -16,14 +17,31 @@ public class FairyRingHelper {
     public static FairyRingHelper get() {
         return fairyRingHelper;
     }
+    public RSInterface fairyRingInterfaceIsValid() {
+        return Interfaces.get(398);
+    }
 
-    public RSInterfaceChild firstFairyRingLetterClockwise = Interfaces.get(398, 19);
-    public RSInterfaceChild firstFairyRingLetterAntiClockwise = Interfaces.get(398, 20);
-    public RSInterfaceChild secondFairyRingLetterClockwise = Interfaces.get(398, 21);
-    public RSInterfaceChild secondFairyRingLetterAntiClockwise = Interfaces.get(398, 22);
-    public RSInterfaceChild thirdFairyRingLetterClockwise = Interfaces.get(398, 23);
-    public RSInterfaceChild thirdFairyRingLetterAntiClockwise = Interfaces.get(398, 24);
-    public RSInterfaceChild confirmFairyRingTeleport = Interfaces.get(398, 26);
+    public RSInterfaceChild firstFairyRingLetterClockwise() {
+        return Interfaces.get(398, 19);
+    }
+    public RSInterfaceChild firstFairyRingLetterAntiClockwise() {
+        return Interfaces.get(398, 20);
+    }
+    public RSInterfaceChild secondFairyRingLetterClockwise() {
+        return Interfaces.get(398, 21);
+    }
+    public RSInterfaceChild secondFairyRingLetterAntiClockwise() {
+        return Interfaces.get(398, 22);
+    }
+    public RSInterfaceChild thirdFairyRingLetterClockwise() {
+        return Interfaces.get(398, 23);
+    }
+    public RSInterfaceChild thirdFairyRingLetterAntiClockwise() {
+        return Interfaces.get(398, 24);
+    }
+    public RSInterfaceChild confirmFairyRingTeleportButton() {
+        return Interfaces.get(398, 26);
+    } 
 
     public RSVarBit firstFairyRingLetterVarbit = RSVarBit.get(3985); // 0 = a 1 = d 2 = c 3 = b
     public RSVarBit secondFairyRingLetterVarbit = RSVarBit.get(3986); // 0 = i 1 = l 2 = k 3 = j
@@ -33,28 +51,29 @@ public class FairyRingHelper {
     public  boolean fairyRingInterfaceIsOpen(){
         return Interfaces.get(398) != null;
     }
-
+    
     /** sets the first fairy ring letter to A **/
     public boolean setFirstFairyRingLetterA() {
-
-        if (firstFairyRingLetterVarbit.getValue() != 0) {
-            if (firstFairyRingLetterVarbit.getValue() == 1) {
-                firstFairyRingLetterClockwise.click();
-                General.sleep(General.randomSD(750, 75));
+            if (firstFairyRingLetterVarbit.getValue() != 0) {
+                if (firstFairyRingLetterVarbit.getValue() == 1) {
+                    final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
+                    firstFairyRingLetterClockwise.click();
+                    General.sleep(General.randomSD(750, 75));
+                }
+                if (firstFairyRingLetterVarbit.getValue() == 2) {
+                    final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
+                    firstFairyRingLetterClockwise.click();
+                    General.sleep(General.randomSD(750, 75));
+                    firstFairyRingLetterClockwise.click();
+                    General.sleep(General.randomSD(750, 75));
+                } else if (firstFairyRingLetterVarbit.getValue() == 3) {
+                    final RSInterfaceChild firstFairyRingLetterAntiClockwise = firstFairyRingLetterAntiClockwise();
+                    firstFairyRingLetterAntiClockwise.click();
+                    General.sleep(General.randomSD(750, 75));
+                }
             }
-            if (firstFairyRingLetterVarbit.getValue() == 2) {
-                firstFairyRingLetterClockwise.click();
-                General.sleep(General.randomSD(750, 75));
-                firstFairyRingLetterClockwise.click();
-                General.sleep(General.randomSD(750, 75));
-            }
-           else if (firstFairyRingLetterVarbit.getValue() == 3) {
-                firstFairyRingLetterAntiClockwise.click();
-                General.sleep(General.randomSD(750, 75));
-            }
-        }
-        General.println("[FAIRY RING NAVIGATION]: First letter 'A' is set");
-        return firstFairyRingLetterVarbit.getValue() == 0;
+            General.println("[FAIRY RING NAVIGATION]: First letter 'A' is set");
+            return firstFairyRingLetterVarbit.getValue() == 0;
     }
 
     /** sets the first fairy ring letter to D **/
@@ -62,16 +81,19 @@ public class FairyRingHelper {
 
         if (firstFairyRingLetterVarbit.getValue() != 1) {
             if (firstFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (firstFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
            else if (firstFairyRingLetterVarbit.getValue() == 2 ) {
+                final RSInterfaceChild firstFairyRingLetterAntiClockwise = firstFairyRingLetterAntiClockwise();
                 firstFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -84,18 +106,20 @@ public class FairyRingHelper {
     public boolean setFirstFairyRingLetterC() {
 
         if (firstFairyRingLetterVarbit.getValue() != 2) {
-
             if (firstFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (firstFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (firstFairyRingLetterVarbit.getValue() == 3 ) {
+                final RSInterfaceChild firstFairyRingLetterAntiClockwise = firstFairyRingLetterAntiClockwise();
                 firstFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -110,16 +134,19 @@ public class FairyRingHelper {
         if (firstFairyRingLetterVarbit.getValue() != 3) {
 
             if (firstFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (firstFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild firstFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 firstFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (firstFairyRingLetterVarbit.getValue() == 0 ) {
+                final RSInterfaceChild firstFairyRingLetterAntiClockwise = firstFairyRingLetterAntiClockwise();
                 firstFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -133,16 +160,19 @@ public class FairyRingHelper {
 
         if (secondFairyRingLetterVarbit.getValue() != 0) {
             if (secondFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (secondFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (secondFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild secondFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -156,16 +186,19 @@ public class FairyRingHelper {
 
         if (secondFairyRingLetterVarbit.getValue() != 1) {
             if (secondFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (secondFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (secondFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild secondFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -179,16 +212,19 @@ public class FairyRingHelper {
 
         if (secondFairyRingLetterVarbit.getValue() != 2) {
             if (secondFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (secondFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (secondFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild secondFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -202,16 +238,19 @@ public class FairyRingHelper {
 
         if (secondFairyRingLetterVarbit.getValue() != 3) {
             if (secondFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (secondFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild secondFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 secondFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (secondFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild secondFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 secondFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -225,16 +264,19 @@ public class FairyRingHelper {
 
         if (thirdFairyRingLetterVarbit.getValue() != 0) {
             if (thirdFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (thirdFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (thirdFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild thirdFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -248,16 +290,19 @@ public class FairyRingHelper {
 
         if (thirdFairyRingLetterVarbit.getValue() != 1) {
             if (thirdFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (thirdFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (thirdFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild thirdFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -271,16 +316,19 @@ public class FairyRingHelper {
 
         if (thirdFairyRingLetterVarbit.getValue() != 2) {
             if (thirdFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (thirdFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (thirdFairyRingLetterVarbit.getValue() == 3) {
+                final RSInterfaceChild thirdFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -294,16 +342,19 @@ public class FairyRingHelper {
 
         if (thirdFairyRingLetterVarbit.getValue() != 3) {
             if (thirdFairyRingLetterVarbit.getValue() == 2) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             if (thirdFairyRingLetterVarbit.getValue() == 1) {
+                final RSInterfaceChild thirdFairyRingLetterClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
                 thirdFairyRingLetterClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
             else if (thirdFairyRingLetterVarbit.getValue() == 0) {
+                final RSInterfaceChild thirdFairyRingLetterAntiClockwise = firstFairyRingLetterClockwise();
                 thirdFairyRingLetterAntiClockwise.click();
                 General.sleep(General.randomSD(750, 75));
             }
@@ -314,8 +365,9 @@ public class FairyRingHelper {
 
     /** confirms fairy rings and initiates the teleport **/
     public boolean confirmFairyRingTeleport() {
-        
+
         if (fairyRingInterfaceIsOpen()) {
+            final RSInterfaceChild confirmFairyRingTeleport = confirmFairyRingTeleportButton();
             confirmFairyRingTeleport.click();
             General.sleep(General.randomSD(2000, 75));
             General.println("[FAIRY RING NAVIGATION]: Confirming fairy ring location.");
