d09e46c5bf68981a859c22f8a26a2f22a976ff53
FlameK <47636477+FlameK@users.noreply.github.com>
2020-02-29 13:43:25 +0100

    Update HandleBanking.java

--- Scripts/MudRuneMaker/Tasks/HandleBanking.java
+++ Scripts/MudRuneMaker/Tasks/HandleBanking.java
@@ -21,7 +21,7 @@ public class HandleBanking implements Task {
 
     public static final RSArea VARROCK_TELEPORT_AREA = new RSArea(new RSTile[]{new RSTile(3206, 3421, 0), new RSTile(3220, 3421, 0), new RSTile(3221, 3438, 0), new RSTile(3205, 3438, 0)});
 
-    public String[] bindingNecklace = {"Binding necklace"};
+    public final String[] bindingNecklace = {"Binding necklace"};
     public String[] inventoryItems = {"Varrock teleport", "Water rune"};
     public String[] pureEssence = {"Pure essence"};
     public String[] staminaPotion = {"Stamina potion(4)", "Stamina potion(3)", "Stamina potion(2)", "Stamina potion(1)"};
@@ -29,6 +29,9 @@ public class HandleBanking implements Task {
     public String[] earthRunes = {"Earth rune"};
 
     public RSVarBit staminaPotionVarBit = RSVarBit.get(25);
+
+    public static int staminaPotionDrank;
+    public static int bindingNecklaceUsed;
 
     @Override
     public Priority priority() {
@@ -63,9 +66,12 @@ public class HandleBanking implements Task {
                 General.println("Deposited all items except Varrock teleport tabs and Water runes.");
                 if (Inventory.find(earthRunes).length > 0) {
                     Banking.depositAllExcept(inventoryItems);
+                    Timing.waitCondition(() -> {
+                        General.sleep(General.randomSD(750, 75));
+                        return Inventory.find(earthRunes).length == 0;
+                            }, General.random(2000, 3000));
                     Antiban.leaveGame();
                 }
-                General.sleep(764, 1450);
                 break;
 
             case REQUIPPING_BINDING_NECKLACE:
@@ -73,39 +79,51 @@ public class HandleBanking implements Task {
                 if (Equipment.getItem(Equipment.SLOTS.AMULET) == null) {
                     Banking.withdraw(1, bindingNecklace);
                     General.println("Withdrawing binding necklace and equipping");
-                    General.sleep(1000, 1546);
+                    General.sleep(General.randomSD(750, 75));
 
-                    ItemEntity bindingNecklace = Entities.find(ItemEntity::new)
-                            .nameEquals("Binding necklace")
-                            .actionsEquals("Wear");
-
-                    if (bindingNecklace != null) {
                         RSItem[] bindingNecklaceInventory = Inventory.find("Binding necklace");
+
                         if (bindingNecklaceInventory != null && bindingNecklaceInventory.length > 0) {
                             if (bindingNecklaceInventory[0].click("Wear")) {
                                 Antiban.waitItemInteractionDelay();
                                 Timing.waitCondition(() -> {
-                                    General.sleep(General.randomSD(3000, 30));
+                                    General.sleep(General.randomSD(750, 75));
                                     return Inventory.find("Binding necklace").length == 0;
-                                }, General.random(782, 1345));
+                                }, General.random(2000, 3000));
                             }
+                            bindingNecklaceUsed += 1;
                             General.println("BANKING: Equipped binding necklace.");
                         }
                     }
                     if (Equipment.getItem(Equipment.SLOTS.AMULET) != null && Inventory.find("Binding necklace").length > 0) {
                         Banking.depositAllExcept(inventoryItems);
-                        General.sleep(980, 1500);
+                        Timing.waitCondition(() -> {
+                                    General.sleep(General.randomSD(750, 75));
+                                    return Inventory.find("Binding necklace").length == 0;
+                                }, General.random(2000, 3000));
                         General.println("BANKING: You are already wearing a binding necklace, banking spare necklace.");
                     }
-                }
 
                 break;
 
             case REDRINKING_STAMINA:
 
                 if (staminaPotionVarBit.getValue() != 1) {
-                    Banking.withdraw(1, staminaPotion);
-                    General.sleep(1000, 1500);
+                    if (Inventory.isFull()) {
+                        Banking.depositAllExcept(inventoryItems);
+                        Timing.waitCondition(() -> {
+                            General.sleep(General.randomSD(750, 75));
+                            return !Inventory.isFull();
+                        }, General.random(2000, 3000));
+                    }
+                   else if (!Inventory.isFull()) {
+                        if (Banking.withdraw(1, staminaPotion)) {
+                            Timing.waitCondition(() -> {
+                                General.sleep(General.randomSD(750, 75));
+                                return Inventory.find(staminaPotion).length == 1;
+                            }, General.random(2000, 3000));
+                        }
+                    }
 
                     ItemEntity staminaPotion = Entities.find(ItemEntity::new)
                             .nameContains("Stamina potion(")
@@ -121,52 +139,60 @@ public class HandleBanking implements Task {
                         if (staminaPotionInventory4 != null && staminaPotionInventory4.length > 0) {
                                 if (staminaPotionInventory4[0].click("Drink")) {
                                     Timing.waitCondition(() -> {
-                                        General.sleep(General.randomSD(1350, 30));
+                                        General.sleep(General.randomSD(750, 75));
                                         return staminaPotionVarBit.getValue() == 1;
-                                    }, General.random(776, 1034));
+                                    }, General.random(2000, 3000));
                                 }
                                 General.println("Drank one dose of Stamina potion (4).");
                             }
                             if (staminaPotionInventory3 != null && staminaPotionInventory3.length > 0) {
                                 if (staminaPotionInventory3[0].click("Drink")) {
                                     Timing.waitCondition(() -> {
-                                        General.sleep(General.randomSD(1350, 30));
+                                        General.sleep(General.randomSD(750, 75));
                                         return staminaPotionVarBit.getValue() == 1;
-                                    }, General.random(776, 1034));
+                                    }, General.random(2000, 3000));
                                 }
                                 General.println("Drank one dose of Stamina potion (3).");
                             }
                             if (staminaPotionInventory2 != null && staminaPotionInventory2.length > 0) {
                                 if (staminaPotionInventory2[0].click("Drink")) {
                                     Timing.waitCondition(() -> {
-                                        General.sleep(General.randomSD(1350, 30));
+                                        General.sleep(General.randomSD(750, 75));
                                         return staminaPotionVarBit.getValue() == 1;
-                                    }, General.random(776, 1034));
+                                    }, General.random(2000, 3000));
                                 }
                                 General.println("Drank one dose of Stamina potion (2).");
                             }
                             if (staminaPotionInventory != null && staminaPotionInventory.length > 0) {
                                 if (staminaPotionInventory[0].click("Drink")) {
                                     Timing.waitCondition(() -> {
-                                        General.sleep(General.randomSD(1350, 30));
+                                        General.sleep(General.randomSD(750, 75));
                                         return staminaPotionVarBit.getValue() == 1;
-                                }, General.random(776, 1034));
+                                }, General.random(2000, 3000));
                             }
                                 General.println("Drank one dose of Stamina potion (1).");
                         }
                     }
+                    staminaPotionDrank += 1;
+
                     Antiban.getReactionTime();
                     Antiban.sleepReactionTime();
                 }
                 if (staminaPotionVarBit.getValue() == 1 && Inventory.find(staminaPotion).length > 0) {
                     Banking.depositAllExcept(inventoryItems);
                     General.println("BANKING: Deposited stamina potion in bank");
-                    General.sleep(745, 1213);
+                    Timing.waitCondition(() -> {
+                        General.sleep(General.randomSD(750, 75));
+                        return Inventory.find(staminaPotion).length == 0;
+                    }, General.random(2000, 3000));
                 }
                 if (Inventory.find("Vial").length > 0) {
                     Banking.deposit(1, "Vial");
+                    Timing.waitCondition(() -> {
+                        General.sleep(General.randomSD(750, 75));
+                        return Inventory.find("Vial").length == 0;
+                    }, General.random(2000, 3000));
                     General.println("BANKING: Deposited vial");
-                    General.sleep(745, 1213);
                 }
                 break;
 
@@ -174,18 +200,24 @@ public class HandleBanking implements Task {
 
                if (Inventory.find(pureEssence).length == 0) {
                        Banking.withdraw(25, pureEssence);
-                       General.sleep(679, 1123);
+                       Timing.waitCondition(() -> {
+                               General.sleep(General.randomSD(750, 75));
+                               return Inventory.find(pureEssence).length == 25;
+                           }, General.random(2000, 3000));
                    }
-                General.println("BANKING: Withdrawn 25 Pure essence.");
-                Antiban.randomMovement();
-                Antiban.leaveGame();
+               General.println("BANKING: Withdrawn 25 Pure essence.");
+               Antiban.randomMovement();
+               Antiban.leaveGame();
                break;
 
             case WITHDRAWING_WATER_TALISMAN:
 
                if (Inventory.find(watertalisman).length == 0) {
                    Banking.withdraw(1, watertalisman);
-                   General.sleep(679, 1123);
+                   Timing.waitCondition(() -> {
+                       General.sleep(General.randomSD(750, 75));
+                       return Inventory.find(watertalisman).length == 1;
+                   }, General.random(2000, 3000));
                }
                 General.println("BANKING: Withdrawn 1 Water talisman.");
                Antiban.randomMovement();
@@ -196,10 +228,7 @@ public class HandleBanking implements Task {
             case BANKING_FAILSAFE:
 
                 Banking.depositAllExcept(inventoryItems);
-                General.sleep(850,1230);
-                if ((!(Inventory.find(pureEssence).length == 25)) || (Inventory.find(watertalisman).length > 1)) {
-
-                }
+                General.sleep(General.randomSD(750, 75));
                 break;
 
                 case CLOSING_BANK:
@@ -244,7 +273,6 @@ public class HandleBanking implements Task {
         if (Inventory.find(watertalisman).length == 0 && Banking.isBankScreenOpen()) {
             return TaskState.WITHDRAWING_WATER_TALISMAN;
         }
-
         if ((Inventory.find(watertalisman).length > 1) || (Inventory.find(staminaPotion).length > 0 && Inventory.find(pureEssence).length > 0) || (Inventory.find(bindingNecklace).length > 0 && Inventory.find(pureEssence).length > 0) || ((!(Inventory.find(pureEssence).length == 25)) && (Inventory.find(staminaPotion).length > 0 || Inventory.find(bindingNecklace).length > 0)) || (!(Inventory.find(pureEssence).length == 25)) && (Inventory.find(watertalisman).length > 1)) {
             return TaskState.BANKING_FAILSAFE;
         }
